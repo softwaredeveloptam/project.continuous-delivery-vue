89f2596633e121bc295c5c1c7eb26b3ceed8d083
Mako Kusuda <mako.kusuda@gmail.com>
2020-05-06 19:42:01 +0900

    Added marker

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <!-- <div>
-      <h2>Search and add a pin</h2>
-      <gmap-autocomplete v-on:place_changed="setPlace"></gmap-autocomplete>
-    </div>-->
-    <button v-on:click="addMarker">Add</button>
-
     <GmapMap
       :zoom="4"
       :center="{ lat: 25.7392, lng: -104.9903 }"
@@ -13,11 +7,11 @@
       style="width: 100%; height: 300px"
     >
       <GmapMarker
-        v-for="location in locations"
-        :key="location.key"
-        :position="location.position"
-        :animation="location.defaultAnimation"
+        v-for="marker in markers"
+        :key="marker.key"
+        :position="marker.position"
         @rightclick="markerRightClicked"
+        icon="http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
       />
     </GmapMap>
   </div>
@@ -27,7 +21,7 @@
 import { gmapApi } from "vue2-google-maps";
 
 export default {
-  name: "GoogleMap",
+  name: "Map",
   mounted() {
     this.getLocations();
   },
@@ -42,39 +36,13 @@ export default {
       this.$store.dispatch("loadMarkers");
     },
     markerRightClicked() {},
-    setPlace() {
-      this.currentPlace = { lat: 0, lng: 0 };
-      console.log(this.currentPlace);
-    },
-    addMarker() {
-      const marker = { lat: 25.7392, lng: -104.9903 };
-      this.markers.push({ position: marker });
-      console.log(this.markers);
-      // if (this.currentPlace) {
-      //   const marker = {
-      //     lat: this.currentPlace.lat,
-      //     lng: this.currentPlace.lng,
-      //   };
-      //   console.log(marker);
-      //   this.markers.push({ position: marker });
-      //   this.places.push(this.currentPlace);
-      //   console.log(this.markers, this.places);
-      //   this.center = marker;
-      //   this.currentPlace = null;
-      // }
-    },
-    // geolocate: function() {
-    //   navigator.geolocation.getCurrentPosition((position) => {
-    //     this.center = {
-    //       lat: position.coords.latitude,
-    //       lun: position.coords.longitude,
-    //     };
-    //   });
-    // },
   },
   data() {
     return {
-      markers: [],
+      markers: [
+        { position: { lat: 25.7392, lng: -104.9903 } },
+        { position: { lat: 26.7392, lng: -105.9903 } },
+      ],
       places: [],
       currentPlace: null,
     };
