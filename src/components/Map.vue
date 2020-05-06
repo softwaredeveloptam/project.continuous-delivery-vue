47320f6229bf49957f4fa5533f2481f0ffd606e0
Mako Kusuda <mako.kusuda@gmail.com>
2020-05-06 18:03:40 +0900

    Initiral commit for Front end

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,14 +1,34 @@
 <template>
-  <GmapMap :zoom="4" :center="{ lat: 25.7392, lng: -104.9903 }" map-type-id="terrain"
-  style="width: 100%; height: 300px">
-    <GmapMarker v-for="location in locations" :key="location.key" :position="location.position" :animation="location.defaultAnimation" @rightclick="markerRightClicked" />
-  </GmapMap>
+  <div>
+    <!-- <div>
+      <h2>Search and add a pin</h2>
+      <gmap-autocomplete v-on:place_changed="setPlace"></gmap-autocomplete>
+      <button v-on:click="addMarker">Add</button>
+    </div>-->
+
+    <GmapMap
+      :zoom="4"
+      :center="{ lat: 25.7392, lng: -104.9903 }"
+      map-type-id="terrain"
+      style="width: 100%; height: 300px"
+      v-on:click="addMarker"
+    >
+      <GmapMarker
+        v-for="location in locations"
+        :key="location.key"
+        :position="location.position"
+        :animation="location.defaultAnimation"
+        @rightclick="markerRightClicked"
+      />
+    </GmapMap>
+  </div>
 </template>
 
 <script>
 import { gmapApi } from "vue2-google-maps";
 
 export default {
+  name: "GoogleMap",
   mounted() {
     this.getLocations();
   },
@@ -23,6 +43,39 @@ export default {
       this.$store.dispatch("loadMarkers");
     },
     markerRightClicked() {},
+    setPlace() {
+      this.currentPlace = { lat: 0, lng: 0 };
+      console.log(this.currentPlace);
+    },
+    addMarker() {
+      if (this.currentPlace) {
+        const marker = {
+          lat: this.currentPlace.lat,
+          lng: this.currentPlace.lng,
+        };
+        console.log(marker);
+        this.markers.push({ position: marker });
+        this.places.push(this.currentPlace);
+        console.log(this.markers, this.places);
+        this.center = marker;
+        this.currentPlace = null;
+      }
+    },
+    // geolocate: function() {
+    //   navigator.geolocation.getCurrentPosition((position) => {
+    //     this.center = {
+    //       lat: position.coords.latitude,
+    //       lun: position.coords.longitude,
+    //     };
+    //   });
+    // },
+  },
+  data() {
+    return {
+      markers: [],
+      places: [],
+      currentPlace: null,
+    };
   },
 };
 </script>
