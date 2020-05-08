7660b956dae2dae1d388e025d2c7126f0c23697c
Mako Kusuda <mako.kusuda@gmail.com>
2020-05-08 21:53:25 +0900

    Location filter works

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -8,9 +8,8 @@
     >
       <GmapMarker
         v-for="marker in markers"
-        :key="marker.key"
-        :position="marker.position"
-        @rightclick="markerRightClicked"
+        :key="marker.id"
+        :position="{ lat: marker.latitude, lng: marker.longitude }"
         :icon="
           'http://maps.google.com/mapfiles/ms/icons/' +
             markerColer(marker.type) +
@@ -18,35 +17,20 @@
         "
       />
     </GmapMap>
+    <button @click="$emit('updateMarkers', markers)" id="hiddenButton">
+      Will hide later
+    </button>
   </div>
 </template>
 
 <script>
 import { gmapApi } from "vue2-google-maps";
-import axios from "axios";
 
 export default {
   name: "Map",
-  mounted() {
-    this.getPlaces().then(() => {
-      this.markers = this.places;
-    }); //get place from database and add marker
-  },
 
   computed: {
-    locations() {
-      return this.$store.state.locations;
-    },
     google: gmapApi,
-  },
-
-  watch: {
-    selectedValue: {
-      handler() {
-        this.updateMarker();
-      },
-      deep: true,
-    },
   },
 
   methods: {
@@ -62,61 +46,7 @@ export default {
         return "red";
       }
     },
-
-    async getPlaces() {
-      const shopLocation = await axios.get("/api/locations");
-      let places = shopLocation.data;
-      places = places.map((shop) => {
-        return {
-          position: { lat: shop.latitude, lng: shop.longitude },
-          type: shop.type,
-          state: shop.state,
-          city: shop.city,
-          highway: shop.highway,
-          name: shop.name,
-          site_name: shop.site_name,
-          //Need to add Amenities and Restaurants
-        };
-      });
-      this.places = places;
-    },
-
-    updateMarker() {
-      this.markers = this.places;
-
-      if (this.selectedValue.selectedState !== "ALL") {
-        this.markers = this.places.filter(
-          (place) => place.state === this.selectedValue.selectedState
-        );
-      }
-
-      if (this.selectedValue.selectedCity !== "ALL") {
-        this.markers = this.markers.filter(
-          (place) => place.city === this.selectedValue.selectedCity
-        );
-      }
-
-      if (this.selectedValue.selectedHighway !== "ALL") {
-        this.markers = this.markers.filter(
-          (place) => place.highway === this.selectedValue.selectedHighway
-        );
-      }
-
-      if (this.selectedValue.selectedTypes.length) {
-        this.markers = this.markers.filter((place) =>
-          this.selectedValue.selectedTypes.includes(place.type)
-        );
-      }
-      //Need to add Amenities and Restaurants
-    },
   },
-  props: ["selectedValue", "submitted"],
-  data() {
-    return {
-      markers: [],
-      places: [],
-      currentPlace: null,
-    };
-  },
+  props: ["markers"],
 };
 </script>
---
+++ src/components/ResultsRegion.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Please Find Your Options Below:</h1>
+    <div v-for="el in markers" :key="el.id" class="LocationBox">
+      <!-- <div class="storeName">Store Name: {{ el.name }}</div>
+      <div class="details">Store Type: {{ el.type }}</div>
+      <div class="details">Store City:{{ el.city }}</div>
+      <div class="details">Highway: {{ el.highway }}</div> -->
+    </div>
+    <!-- <h4>{{ markers }}</h4> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResultsRegion",
+  props: {
+    markers: Array,
+  },
+};
+</script>
+
+<style scoped>
+.storeName {
+  font-size: 18;
+}
+.details {
+  font-size: 14;
+}
+.LocationBox {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+
+  margin-top: 20px;
+  outline: 1px solid teal;
+  outline-offset: 3 px;
+}
+</style>
