e612c5fac51e9ad6176c9d40dcf1ed7a619cabc1
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-05-08 15:07:40 +0900

    Set up some tests for FilteRegion

--- tests/unit/FilterRegion.spec.js
+++ tests/unit/FilterRegion.spec.js
@@ -16,4 +16,24 @@ describe("FilterRegion.vue", () => {
     wrapper.destroy();
     wrapper = null;
   });
+
+  it("can filter based on State", () => {});
+
+  it("removes all other selections outside of state", () => {});
+
+  it("shows a list of Cities when State is selected", () => {});
+
+  it("can filter based on City selection", () => {});
+
+  it("when a city and selected are selected, I can see a highways list", () => {});
+
+  it("when I select a highway, all other locations disappear", () => {});
+
+  it("Should only see travel stops when selected", () => {});
+
+  it("Should only see country stops when selected", () => {});
+
+  it("Should show both travel and country stores when I select them", () => {});
+
+  it("Should show all locations country and travel stops when each box is unselected", () => {});
 });
